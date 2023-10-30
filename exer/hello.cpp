06993b390ec7036ccf0b8697af653d4b718cade0
ThiagoMarx <anonymoussbc@proton.me>
2023-10-30 01:19:51 -0300

    Hello, World.

---
+++ exer/hello.cpp
@@ -0,0 +1,14 @@
+// g++ hello.cpp -o hello && ./hello
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello, World!!!" << endl;
+	// cout << "Hello, World!!!\n";
+
+return 0;}
+// g++ hello.cpp -o hello && ./hello
