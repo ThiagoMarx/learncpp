403a021de859c6055fc551c532610000b2ec1173
ThiagoMarx <anonymoussbc@proton.me>
2023-10-30 00:58:07 -0300

    Estrutura básica em C++.

---
+++ test/base.cpp
@@ -0,0 +1,13 @@
+// g++ base.cpp -o base && ./base
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	
+
+return 0;}
+// g++ base.cpp -o base && ./base
